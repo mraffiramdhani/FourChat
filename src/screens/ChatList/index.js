38dee2f297602025dcdc4e65b96b0146416e3542
mraffiramdhani <raffi.programming@gmail.com>
2020-01-31 19:18:26 +0700

    Creating Chat List Prototype

--- src/screens/ChatList/index.js
+++ src/screens/ChatList/index.js
@@ -1,6 +1,23 @@
 import React, {useState, useEffect} from 'react';
 import SafeAreaView from 'react-native-safe-area-view';
-import { StyleSheet, View, Text } from 'react-native';
+import normalize from 'react-native-normalize';
+import { StyleSheet, View, Text, FlatList } from 'react-native';
+import { Button, IconButton, List, Divider } from 'react-native-paper';
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
 
 const styles = StyleSheet.create({
 	safeArea: {flex: 1},
@@ -10,14 +27,61 @@ const styles = StyleSheet.create({
 		backgroundColor: 'white',
 		justifyContent: 'center',
 		alignItems: 'center',
-	}
-})
+	},
+	headerWrapper: {
+		flexDirection: 'row',
+		height: normalize(50, 'height'),
+		width: '100%',
+		backgroundColor: '#289C8E',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	title: {
+		color: 'white',
+		fontSize: 24,
+		fontFamily: 'Nunito-Bold',
+	},
+	bodyWrapper: {
+		flex: 1,
+		flexDirection: 'column',
+		width: '100%',
+	},
+});
+
+const ChatItem = ({title, id}) => {
+	return (
+		<List.Item title={title} description={id} left={props => <List.Icon {...props} icon="folder" />} />
+	);
+};
 
 const ChatList = (props) => {
+	const renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "86%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "14%"
+        }}
+      />
+    );
+  };
+
   return (
   	<SafeAreaView style={styles.safeArea}>
 	    <View style={styles.root}>
-	      <Text>ChatList</Text>
+	      <View style={styles.headerWrapper}>
+	      	<Text style={styles.title}>Conversation</Text>
+	      </View>
+	      <View style={styles.bodyWrapper}>
+	      	<FlatList
+	      		data={DATA}
+	      		itemSeparatorComponent={renderSeparator}
+        		renderItem={({ item }) => <ChatItem title={item.title} id={item.id} />}
+        		keyExtractor={item => item.id}
+	      	/>
+	      </View>
 	    </View>
     </SafeAreaView>
   );
